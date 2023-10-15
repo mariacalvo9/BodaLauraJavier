b231aed39b8faf9c0f11388a9c38e88328b4d0a2
mariacalvo9 <147953728+mariacalvo9@users.noreply.github.com>
2023-10-15 16:06:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const { MongoClient } = require('mongodb');
 
 
 // Configuración de la conexión a MongoDB
-const uri = 'mongodb://mongo:PIWMwSqbh2bO4LFKaa65@containers-us-west-204.railway.app:7084';
+const uri = 'mongodb+srv://vercel-admin-user:<password>@cluster0.80amdms.mongodb.net/?retryWrites=true&w=majority';
 //const uri = 'mongodb://mongo:PIWMwSqbh2bO4LFKaa65@containers-us-west-204.railway.app:7084';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
