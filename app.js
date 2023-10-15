cd5b838835e628c63756f78ea29180351493441b
mariacalvo9 <147953728+mariacalvo9@users.noreply.github.com>
2023-10-15 18:04:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const { MongoClient } = require("mongodb");
-var express = require('express');
-var path = require('path');
+var express = require('express');var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser');
